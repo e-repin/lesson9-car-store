db733b74f25c255a2dca6e5c95d2498f34445c17
e.repin <mr.evgeniy.repin@gmail.com>
2020-03-19 21:31:40 +0200

    dasd

--- CarStorage/CarStorage/Car.cs
+++ CarStorage/CarStorage/Car.cs
@@ -6,9 +6,24 @@ namespace CarStorage
 {
     public class Car
     {
+        private int _price = 0;
         public string Color { get; set; }
-
-        public int Price { get; set; }
+         
+        public int Price
+        { 
+            get { return _price; }
+            set
+            {
+                if(value > 0 )
+                {
+                    _price = value;
+                }
+                else
+                {
+                    Console.WriteLine($"Ti pidor {value}");
+                }
+            }
+        }
 
         public string Name { get; set; }
 
---
+++ CarStorage/CarStorage/NonCargoCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarStorage
+{
+    class NonCargoCar : Car
+    {
+        public void Check ()
+        {
+
+            GetSale
+        }
+    }
+}
