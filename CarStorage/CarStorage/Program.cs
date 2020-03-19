833aa2e241c14a6fb65fe1733f3d6eb9b62b887b
e.repin <mr.evgeniy.repin@gmail.com>
2020-03-19 20:31:10 +0200

    dasd

--- CarStorage/CarStorage/Program.cs
+++ CarStorage/CarStorage/Program.cs
@@ -6,7 +6,22 @@ namespace CarStorage
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car[] cars = new Car[3];
+            for (var i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine("Input Name");
+                cars[i].Name = Console.ReadLine();
+
+                Console.WriteLine("Input Price");
+                cars[i].Price = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Input Color");
+                cars[i].Color = Console.ReadLine();
+
+            }
+
+
+            Console.WriteLine($"Sourse prise {cars[0].Price} ; Price after sale: {cars[0].GetSale()}");
         }
     }
 }
